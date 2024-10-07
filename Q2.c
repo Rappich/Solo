5e21a9d70ee8fbe9225386301d017cb70cc13490
Rappich <fredrik.martensson@chasacademy.se>
2024-10-07 21:08:00 +0200

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct Books
+{
+    char title [40];
+    char author [40];
+    int pages;
+    
+}books;
+
+int main ()
+
+{
+
+}
+
+
+
+/*Uppgift 2: Hantera böcker och söka efter en författare
+Beskrivning: Definiera en struct för att lagra information om en bok (titel, författare, antal sidor).
+	•	Skapa en array med 5 böcker och fyll den med information.
+	•	Skriv en funktion som söker efter alla böcker av en viss författare och skriver ut deras titlar.
+	•	Använd loopar för att iterera genom arrayen och skriv ut informationen.
+Koncept som tränas: Structs, arrays, loopar, funktioner.*/
