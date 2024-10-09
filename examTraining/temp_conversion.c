7ef256e315ad77049355ff2631cfa81ab5ec8dd1
Rappich <fredrik.martensson@chasacademy.se>
2024-10-09 18:13:13 +0200

    more tasks

--- examTraining/temp_conversion.c
+++ examTraining/temp_conversion.c
@@ -1,32 +1,18 @@
-// temp_conversions.c
 #include "temp_conversion.h"
 
-// Convert Celsius to Fahrenheit
+// Funktioner för temperaturkonvertering
 float celsiusToFahrenheit(float celsius) {
-    return (celsius * 9.0 / 5.0) + 32.0;
+    return (celsius * 9.0 / 5.0) + 32.0;  // Konverterar Celsius till Fahrenheit
 }
 
-// Convert Fahrenheit to Celsius
-float fahrenheitToCelsius(float fahrenheit) {
-    return (fahrenheit - 32.0) * 5.0 / 9.0;
-}
-
-// Convert Celsius to Kelvin
 float celsiusToKelvin(float celsius) {
-    return celsius + 273.15;
+    return celsius + 273.15;  // Konverterar Celsius till Kelvin
 }
 
-// Convert Kelvin to Celsius
+float fahrenheitToCelsius(float fahrenheit) {
+    return (fahrenheit - 32) * 5.0 / 9.0;  // Konverterar Fahrenheit till Celsius
+}
+
 float kelvinToCelsius(float kelvin) {
-    return kelvin - 273.15;
-}
-
-// Convert Fahrenheit to Kelvin
-float fahrenheitToKelvin(float fahrenheit) {
-    return (fahrenheit - 32.0) * 5.0 / 9.0 + 273.15;
-}
-
-// Convert Kelvin to Fahrenheit
-float kelvinToFahrenheit(float kelvin) {
-    return (kelvin - 273.15) * 9.0 / 5.0 + 32.0;
+    return kelvin - 273.15;  // Konverterar Kelvin till Celsius
 }
